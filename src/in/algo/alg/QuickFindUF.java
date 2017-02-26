0074825a49cc1f60bb020ff979eeb5db2df1c384
Nishant <nishant.awasthi1@globallogic.com>
2017-02-26 20:29:08 +0530

    committing unsaved changes

---
+++ src/in/algo/alg/QuickFindUF.java
@@ -0,0 +1,29 @@
+/**
+ * 
+ */
+package in.algo.alg;
+
+/**
+ * @author nishant.awasthi
+ *
+ */
+public class QuickFindUF {
+
+	/**
+	 * 
+	 */
+	public QuickFindUF(int n) {
+		
+	}
+	
+	
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/in/algo/alg/QuickUnionUF.java
@@ -0,0 +1,36 @@
+/**
+ * 
+ */
+package in.algo.alg;
+
+/**
+ * @author nishant.awasthi1
+ *
+ */
+public class QuickUnionUF {
+	private int[] id;
+
+	/**
+	 * 
+	 */
+	public QuickUnionUF(int n) {
+		id = new int[n];
+		for (int i = 0; i < n; i++) {
+			id[i] = i;
+
+		}
+	}
+	
+	private int root(int i){
+		
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
